ec3aaeeb4357e8baba787757429201e6d3a05e28
Taaha Bajwa <taaha.s.bajwa@gmail.com>
2023-07-16 01:53:11 +0500

    hooha

--- src/pages/chatpdf.js
+++ src/pages/chatpdf.js
@@ -2,34 +2,21 @@ import React from 'react';
 
 function Chatpdf() {
 
-  const ref = React.useRef();
-  const [height, setHeight] = React.useState("0px");
-  const onLoad = () => {
-    setHeight(ref.current.contentWindow.document.body.scrollHeight + "px");
-  };
-
   return (
     <>
-    <div>  
-        {/* <iframe
-          src="https://darthpanda-chatpdf-app.hf.space"
-          title="Chat PDF Application"
-          style={{ width: '100%', height: '100%', display: 'block' }}
-          ></iframe> */}
-
-  <iframe
-      ref={ref}
-      onLoad={onLoad}
-      id="myFrame"
-      title="Chat PDF Application"
-      src="https://darthpanda-chatpdf-app.hf.space"
-      width="100%"
-      // height={height}
-      style={{
-        width: "100%",
-        overflow: "auto",
-      }}
-    ></iframe>
+    <div style={{ height: '100vh' }}>  
+          <iframe
+              id="myFrame"
+              title="Chat PDF Application"
+              src="https://darthpanda-chatpdf-app.hf.space"
+              width="100%"
+              height="100%"
+              style={{
+                width: "100%",
+                height:"100%",
+                overflow: "auto",
+              }}
+            ></iframe>
     </div>
 
     </>
